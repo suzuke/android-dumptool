087fb8cfca4ab4e284ae35a8fb2ede8ec66f6995
chiachenghuang <suzuke@hotmail.com>
2018-08-13 15:13:01 +0800

    fix "only position independent executables (pie) are supported"

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,10 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := dumptool
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 LOCAL_SRC_FILES := dumptool.c  util.c 
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 LOCAL_MODULE := dumpmem
 LOCAL_SRC_FILES := dumpmem.c  util.c 
 include $(BUILD_EXECUTABLE)
